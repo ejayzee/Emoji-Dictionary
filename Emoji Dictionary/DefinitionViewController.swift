5c1fb0a70a32db625111bad341e44b2aacd7337f
EMMANUEL J ZAFRA <ejayzee@mac.com>
2016-10-19 15:57:47 -0700

    Segue from view controller to the next

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by EMMANUEL J ZAFRA on 10/19/16.
+//  Copyright © 2016 EMMANUEL J ZAFRA. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    var emoji = "NO EMOJI"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        emojiLabel.text = emoji
+        
+        //definitionLabel.text
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
